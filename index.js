06341bdcebad3f150148b3c66bd7b4734498c310
Katelyn <katelynjewel@gmail.com>
2021-09-27 15:52:25 -0700

    commit tree bf7af3f35d6b4326dd776349948b069e101375a5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function razzle() {
+    console.log("You could razzle that.");
+}
+// razzle();
+
+
+function tallahasse(placeName = "Denver") {
+    console.log(`If you want to go to ${placeName}`)
+}
+
+// tallahasse("Kauai");
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective() {
+    return function (adjective = "special", exclaim = "*") {
+        return `You are ${exclaim}${adjective}${exclaim}!`
+    }
+}
